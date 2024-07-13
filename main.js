a8a385b346b3744bb35bbc70ee70367dcd18a75d
Bohak Oleh <eddrfgthyjn@gmail.com>
2024-07-13 17:15:19 +0300

    .

---
+++ main.js
@@ -0,0 +1,21 @@
+import { Autoplay, Pagination } from "./node_modules/swiper/modules";
+import Swiper from "./node_modules/swiper";
+import "./node_modules/swiper/swiper-bundle.min.css";
+
+new Swiper(".hero-swiper-container", {
+  modules: [Autoplay, Pagination],
+
+  slidesPerView: 1,
+  spaceBetween: 10,
+  loop: true,
+
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
