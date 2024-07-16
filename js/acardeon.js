a1e8a04d52c1267850efc80f7dde8454c573f5c7
Oleh Bohak <eddrfgthyjn@gmail.com>
2024-07-16 16:43:42 +0300

    Internet-TV page ready

---
+++ js/acardeon.js
@@ -0,0 +1,11 @@
+const parent = document.querySelector(".question-container");
+
+parent.addEventListener("click", (event) => {
+  const target = event.target;
+
+  const backdrop = target.closest(".question-item");
+
+  backdrop
+    .querySelector(".question-item-acardeon-text-JS")
+    .classList.toggle("is-active");
+});
