6b45d9722baf0a53bbb6490fdf6b4484abe68c1c
Bohak Oleh <eddrfgthyjn@gmail.com>
2024-07-22 14:06:56 +0300

    Catalog-acardeon

--- js/acardeon2.js
+++ js/acardeon2.js
@@ -1,16 +1,56 @@
 const parent = document.querySelector(".akuvox-acardeon-container");
+const akuvoxItems = Array.from(
+  parent.querySelectorAll(".akuvox-acardeon-item")
+);
+let currentIndex = 0;
 
-parent.addEventListener("click", (event) => {
-  const akuvoxItem = event.target.closest(".akuvox-acardeon-item");
-  if (!akuvoxItem) return;
-
-  // Тоглінг класу is-active для знайденого елемента та його дочірніх елементів
-  akuvoxItem.classList.toggle("is-active");
-
+function addClassToElement(element) {
+  element.classList.add("is-active");
   ["title", "icon", "text"].forEach((className) => {
-    const element = akuvoxItem.querySelector(
+    const childElement = element.querySelector(
       `.akuvox-acardeon-item-${className}`
     );
-    if (element) element.classList.toggle("is-active");
+    if (childElement) childElement.classList.add("is-active");
   });
-});
+
+  // Додаємо смужку
+  const progressBar = document.createElement("div");
+  progressBar.classList.add("progress-bar");
+  // element.appendChild(progressBar);
+
+  // Видаляємо смужку через 3 секунди
+  setTimeout(() => {
+    progressBar.remove();
+  }, 3000);
+}
+
+function removeClassFromElement(element) {
+  element.classList.remove("is-active");
+  ["title", "icon", "text"].forEach((className) => {
+    const childElement = element.querySelector(
+      `.akuvox-acardeon-item-${className}`
+    );
+    if (childElement) childElement.classList.remove("is-active");
+  });
+}
+
+function processNextItem() {
+  const currentElement = akuvoxItems[currentIndex];
+
+  // Додаємо клас is-active до поточного елемента
+  addClassToElement(currentElement);
+
+  // Через 3 секунди видаляємо клас is-active
+  setTimeout(() => {
+    removeClassFromElement(currentElement);
+
+    // Переходимо до наступного елемента
+    currentIndex = (currentIndex + 1) % akuvoxItems.length;
+
+    // Переходимо до наступного елемента через 0.5 секунди після закриття попереднього
+    setTimeout(processNextItem, 50);
+  }, 6000);
+}
+
+// Починаємо процес
+processNextItem();
