ea75ec2f63b95ac50a986542c05b272901a0ed44
Bohak Oleh <eddrfgthyjn@gmail.com>
2024-08-06 16:35:17 +0300

    .

--- js/we-can-swiper.js
+++ js/we-can-swiper.js
@@ -12,3 +12,33 @@ new Swiper(".we-can-swiper-container", {
     },
   },
 });
+
+new Swiper(".we-can-swiper-container-short", {
+  slidesPerView: 1.1,
+  spaceBetween: 12,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 0,
+    },
+  },
+});
+
+new Swiper(".we-can-swiper-container-short2", {
+  slidesPerView: 1.1,
+  spaceBetween: 12,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 0,
+    },
+  },
+});
