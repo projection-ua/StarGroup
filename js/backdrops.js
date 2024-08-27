9effdd46da8c395221c81938f6ec965adc7469a4
Bohak Oleh <eddrfgthyjn@gmail.com>
2024-08-27 12:40:19 +0300

    ю

--- js/backdrops.js
+++ js/backdrops.js
@@ -1,12 +1,23 @@
 // Header tel numbers backdrop
-
 const telBackdropBtn = document.querySelector(".address-tel-number");
 const telBackdrop = document.querySelector(".addres-tel-number-backdrop-list");
 const telNumbersContainer = document.querySelector(
   ".header-tel-numbers-container"
 );
 
-telBackdropBtn.addEventListener("click", () => {
-    telBackdrop.classList.toggle("active");
-    telNumbersContainer.classList.toggle("active");
+// Додавання або зняття класу active при кліку на кнопку
+telBackdropBtn.addEventListener("click", (event) => {
+  telBackdrop.classList.toggle("active");
+  telNumbersContainer.classList.toggle("active");
+  event.stopPropagation(); // Запобігає спрацюванню події на документі
+});
+
+// Закриття при кліку за межами контейнера
+document.addEventListener("click", (event) => {
+  const isClickInside = telNumbersContainer.contains(event.target);
+
+  if (!isClickInside) {
+    telBackdrop.classList.remove("active");
+    telNumbersContainer.classList.remove("active");
+  }
 });
